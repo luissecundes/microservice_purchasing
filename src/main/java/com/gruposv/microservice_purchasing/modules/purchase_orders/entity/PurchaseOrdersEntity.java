36ebf29bafbb74b4fa6ffbfbf711924707fa3527
luissecundes <luissecundes@gmail.com>
2025-05-30 17:27:58 -0300

    criação de entidade purchase order e enum

---
+++ src/main/java/com/gruposv/microservice_purchasing/modules/purchase_orders/entity/PurchaseOrdersEntity.java
@@ -0,0 +1,62 @@
+package com.gruposv.microservice_purchasing.modules.purchase_orders.entity;
+
+import com.gruposv.microservice_purchasing.modules.purchase_orders.enums.PurchaseOrderStatus;
+import com.gruposv.microservice_purchasing.modules.supplier.entity.SupplierEntity;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.Where;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.time.LocalDate;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "tb_purchase_orders")
+@Where(clause = "deleted_at IS NULL")
+public class PurchaseOrdersEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "order_number", length = 50, nullable = false)
+    private String orderNumber;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "supplier_id", nullable = false)
+    private SupplierEntity supplier;
+
+    @Column(name = "order_date")
+    private LocalDate orderDate;
+
+    @Column(name = "expected_delivery_date")
+    private LocalDate expectedDeliveryDate;
+
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "status", length = 20)
+    private PurchaseOrderStatus status;
+
+
+    @Column(name = "total_amount", precision = 12, scale = 2)
+    private BigDecimal totalAmount;
+
+    @Column(name = "currency", length = 10)
+    private String currency;
+
+    @Column(name = "payment_terms", length = 100)
+    private String paymentTerms;
+
+    @Column(name = "created_at")
+    private LocalDateTime createdAt;
+
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
+}
---
+++ src/main/java/com/gruposv/microservice_purchasing/modules/purchase_orders/enums/PurchaseOrderStatus.java
@@ -0,0 +1,9 @@
+package com.gruposv.microservice_purchasing.modules.purchase_orders.enums;
+
+public enum PurchaseOrderStatus {
+    CRIADO,
+    APROVADO,
+    ENVIADO,
+    RECEBIDO,
+    CANCELADO
+}
