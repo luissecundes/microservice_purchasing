bb60cd922530cea8acd0b6e814d8b29bd65f8e3e
luissecundes <luissecundes@gmail.com>
2025-05-30 18:20:42 -0300

    criação de service

---
+++ src/main/java/com/gruposv/microservice_purchasing/modules/purchase_orders/service/PurchaseOrdersService.java
@@ -0,0 +1,21 @@
+package com.gruposv.microservice_purchasing.modules.purchase_orders.service;
+
+import com.gruposv.microservice_purchasing.modules.purchase_orders.entity.PurchaseOrdersEntity;
+import com.gruposv.microservice_purchasing.modules.purchase_orders.repository.PurchaseOrdersRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class PurchaseOrdersService {
+
+    private final PurchaseOrdersRepository purchaseOrdersRepository;
+
+    @Autowired
+    public PurchaseOrdersService(PurchaseOrdersRepository purchaseOrdersRepository) {
+        this.purchaseOrdersRepository = purchaseOrdersRepository;
+    }
+ 
+    public PurchaseOrdersEntity savePurchaseOrder(PurchaseOrdersEntity purchaseOrder) {
+        return purchaseOrdersRepository.save(purchaseOrder);
+    }
+}
