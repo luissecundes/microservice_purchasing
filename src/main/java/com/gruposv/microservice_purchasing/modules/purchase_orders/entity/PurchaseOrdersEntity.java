2394d343be533fb225581f52a544ff018fa8bf72
Luis Henrique <luissecundes@gmail.com>
2025-06-11 17:56:23 -0300

    add comentário

--- src/main/java/com/gruposv/microservice_purchasing/modules/purchase_orders/entity/PurchaseOrdersEntity.java
+++ src/main/java/com/gruposv/microservice_purchasing/modules/purchase_orders/entity/PurchaseOrdersEntity.java
@@ -20,7 +20,7 @@ import java.time.LocalDate;
 @Entity
 @Table(name = "tb_purchase_orders")
 public class PurchaseOrdersEntity {
-
+// comentário
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
